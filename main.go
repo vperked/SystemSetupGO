0d857f9b65c040cca5f6782a40fc65af1d5a6f80
vperked <perked@riseup.net>
2024-04-08 20:35:21 -0400

    New Functions!

--- main.go
+++ main.go
@@ -7,15 +7,20 @@ import (
 
 func main() {
 	var command string
+	var invalid string = "Invalid Command"
 	fmt.Println("Please enter a command.")
 	fmt.Scan(&command)
-
 	if command == "reboot" {
-		rebootCMD()
+		speedTestCMD()
 	} else if command == "update" {
 		updateCMD()
+	} else if command == "install" {
+		installCMD()
+	} else if command == "speedtest" {
+		speedTestCMD()
 	} else {
-		fmt.Println("Invalid Command.")
+		fmt.Println(invalid)
+		fmt.Scanln()
 	}
 }
 
@@ -31,7 +36,29 @@ func updateCMD() {
 	cmd := exec.Command("apt", "update")
 	out, err := cmd.Output()
 	if err != nil {
-		fmt.Print("There was an error, ", err)
+		fmt.Print("There was an error: ", err)
 	}
 	fmt.Println("Command Output:", string(out))
+}
+
+func installCMD() {
+	packages := []string{"nload", "wireshark", "tshark"}
+	for _, pkg := range packages {
+		cmd := exec.Command("apt", "install", pkg)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println("There was an error: ", err)
+		}
+		fmt.Println("Command Output:", string(out))
+	}
+
+}
+
+func speedTestCMD() {
+	cmd := exec.Command("bash", "-c", "curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash && sudo apt-get install speedtest")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("There was an error: ", err)
+	}
+	fmt.Println("Command Output: ", string(out))
 }
