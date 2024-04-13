7dc125117b3a95bcd843821636f3c9e4b6f411d0
vperked <perked@riseup.net>
2024-04-13 13:13:00 -0400

    Removed useless functions and added a option text.

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ func main() {
 	var command string
 	var invalid string = "Invalid Command"
 	fmt.Println("Please enter a command.")
+	fmt.Println("Your options are, update, install, speedtest.")
 	fmt.Scan(&command)
 	if command == "root" {
-		rebootCMD()
 	} else if command == "update" {
 		updateCMD()
 	} else if command == "install" {
@@ -21,14 +21,6 @@ func main() {
 	} else {
 		fmt.Println(invalid)
 		fmt.Scanln()
-	}
-}
-
-func rebootCMD() {
-	cmd := exec.Command("reboot")
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println("There was an error, ", err)
 	}
 }
 
@@ -44,18 +36,19 @@ func updateCMD() {
 func installCMD() {
 	packages := []string{"nload", "wireshark", "tshark", "tcpdump", "ipset", "nmap", "net-tools"}
 	for _, pkg := range packages {
-		cmd := exec.Command("apt", "install", pkg, "-s")
+		cmd := exec.Command("apt", "install", pkg, "-y")
+		cmd.Stdin = nil
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			fmt.Println("There was an error: ", err)
+			continue
 		}
 		fmt.Println("Command Output:", string(out))
 	}
-
 }
 
 func speedTestCMD() {
-	cmd := exec.Command("bash", "-c", "curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash && sudo apt-get install speedtest")
+	cmd := exec.Command("bash", "-c", "curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash && sudo apt-get install speedtest -y")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("There was an error: ", err)
