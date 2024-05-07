0f38e85d6fbfe64f35fd09e872c74ea50692ab7e
vperked <perked@riseup.net>
2024-05-06 22:18:39 -0400

    Added port dropping.

--- src/main.go
+++ src/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os/exec"
 	"systemsetupgolang/src/packages"
 )
 
@@ -12,60 +10,20 @@ func main() {
 	var command string
 	var invalid string = "Invalid Command"
 	fmt.Println("Please enter a command.")
-	fmt.Println("Input a command, a list can be found in cmds.txt!")
+	fmt.Println("Your commands are, setup, openvpn, hostname.")
 	fmt.Scan(&command)
-	if command == "update" {
-		updateCMD()
-	} else if command == "setup" {
-		updateCMD()
-		installCMD()
+	switch command {
+	case "setup":
+		packages.UpdateCMD()
+		packages.InstallCMD()
 		packages.SpeedTestCMD()
 		packages.SetHostname()
 		packages.WhiteList()
-	} else if command == "install" {
-		installCMD()
-	} else if command == "speedtest" {
-		packages.SpeedTestCMD()
-	} else if command == "nginx" {
-		packages.InstallNginx()
-	} else if command == "hostname" {
-		packages.SetHostname()
-	} else if command == "openvpn" {
+	case "openvpn":
 		packages.OpenVPN()
-	} else {
+	case "hostname":
+		packages.SetHostname()
+	case "":
 		fmt.Println(invalid)
-		fmt.Scanln()
-	}
-}
-
-func updateCMD() {
-	cmd := exec.Command("apt", "update", "-y")
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Print("There was an error: ", err)
-		return
-	}
-	fmt.Println("Command Output:", string(out))
-
-	upgrade := exec.Command("apt", "upgrade", "-y")
-	output, err := upgrade.CombinedOutput()
-	if err != nil {
-		log.Fatal("Couldnt upgrade", err)
-		return
-	}
-	fmt.Println("Command output:", string(output))
-}
-
-func installCMD() {
-	packages := []string{"nload", "wireshark", "tshark", "tcpdump", "ipset", "nmap", "net-tools"}
-	for _, pkg := range packages {
-		cmd := exec.Command("apt", "install", pkg, "-y")
-		cmd.Stdin = nil
-		out, err := cmd.CombinedOutput()
-		if err != nil {
-			fmt.Println("There was an error: ", err)
-			return
-		}
-		fmt.Println("Command Output:", string(out))
 	}
 }
--- src/packages/mainsetup.go
+++ src/packages/mainsetup.go
@@ -32,4 +32,57 @@ func WhiteList() {
 		log.Fatal(err)
 	}
 	fmt.Println(string(out))
+	fmt.Println("Would you like to drop the port?")
+	var drop string
+	fmt.Scan(&drop)
+	if drop == "yes" {
+		dropPort()
+	} else {
+		return
+	}
+}
+
+func dropPort() {
+	fmt.Println("What port would you like to drop?")
+	var port string
+	fmt.Scan(&port)
+	cmd := exec.Command("iptables", "-A", "INPUT", "-p", "tcp", "--dport", port, "-j", "DROP")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(out)
+
+}
+
+func UpdateCMD() {
+	cmd := exec.Command("apt", "update", "-y")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Print("There was an error: ", err)
+		return
+	}
+	fmt.Println("Command Output:", string(out))
+
+	upgrade := exec.Command("apt", "upgrade", "-y")
+	output, err := upgrade.CombinedOutput()
+	if err != nil {
+		log.Fatal("Couldnt upgrade", err)
+		return
+	}
+	fmt.Println("Command output:", string(output))
+}
+
+func InstallCMD() {
+	packages := []string{"nload", "wireshark", "tshark", "tcpdump", "ipset", "nmap", "net-tools"}
+	for _, pkg := range packages {
+		cmd := exec.Command("apt", "install", pkg, "-y")
+		cmd.Stdin = nil
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println("There was an error: ", err)
+			return
+		}
+		fmt.Println("Command Output:", string(out))
+	}
 }
